226ff16cd2abacb9597bd9b111150693a482c2d0
KadoreK <haltaskadir@gmail.com>
2016-10-24 21:46:24 +0300

    Add project files.

---
+++ Yolculuk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Yolculuk.Startup))]
+namespace Yolculuk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
